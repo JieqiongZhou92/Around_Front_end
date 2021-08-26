de8f5c0f6ffbfa45cf6ba6462414742adfbad545
SebastianGao <53056387+Sebastiangao0219@users.noreply.github.com>
2021-08-25 23:56:18 -0500

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,38 @@
+import logo from '../assets/images/logo.svg';
+import '../styles/App.css';
+import React, {useState}from "react";
+import TopBar from "./TopBar";
+import { TOKEN_KEY } from "../constants";
+import Main from "./Main"
+
+function App() {
+
+    const [isLoggedIn, setIsLoggedIn] = useState(
+        localStorage.getItem(TOKEN_KEY) ? true : false
+    );
+
+    const logout = () => {
+        console.log("log out");
+        localStorage.removeItem(TOKEN_KEY);
+        setIsLoggedIn(false);
+    };
+
+    const loggedIn = (token) => {
+        if (token) {
+            localStorage.setItem(TOKEN_KEY, token);
+            setIsLoggedIn(true);
+        }
+    };
+
+    return (
+        <div className="App">
+            <TopBar
+                isLoggedIn={true}
+                handleLogout={logout}
+            />
+            <Main isLoggedIn={isLoggedIn} handleLoggedIn={loggedIn}/>
+        </div>
+    );
+}
+
+export default App;
